af21b79bd02c1fdb2b2acf6d6f065fd11bf7c971
remmarziano <saunaflor9@gmail.com>
2025-02-11 16:18:10 -0600

    Add overflow in riscv.rs and int/shift.rs

---
+++ src/riscv.rs
@@ -0,0 +1,14 @@
+    pub extern "C" fn __mulsi3(a: u32, b: u32) -> u32 {
+        let (mut a, mut b) = (a, b);
+        let mut r: u32 = 0;
+
+        while a > 0 {
+            if a & 1 > 0 {
+                r = r.wrapping_add(b);
+            }
+            a >>= 1;
+            b <<= 1;
+        }
+
+        r
+    }
