95f0b383fa675b36828930281365cc4671f8f34d
Connor Fitzgerald <saunaflor9@gmail.com>
2025-02-13 17:23:34 -0600

    Fix/pubsub client

--- build.rs
+++ build.rs
@@ -1,1 +1,7 @@
 use std::{collections::HashMap, path::Path};
+
+const LIB: &str = "libtangent_ffi_sys";
+const SUPPORTED_PLATFORMS: &[(&str, &str, &str)] = &[
+    ("apple", "x86_64-apple-darwin", "dylib"),
+    ("linux", "x86_64-unknown-linux-gnu", "so"),
+];
