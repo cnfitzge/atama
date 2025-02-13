192d8795fdb3a8ce945de7aacfd020eadf3fedac
Connor Fitzgerald <saunaflor9@gmail.com>
2025-02-13 17:29:21 -0600

    fix build.rs

--- build.rs
+++ build.rs
@@ -5,3 +5,12 @@ const SUPPORTED_PLATFORMS: &[(&str, &str, &str)] = &[
     ("apple", "x86_64-apple-darwin", "dylib"),
     ("linux", "x86_64-unknown-linux-gnu", "so"),
 ];
+const FFI_TOOLCHAIN_VERSION: &str = "1.76.0";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let current_dir = std::env::current_dir()?.canonicalize()?;
+
+    // Generate IDL types from 'res/drift.json'
+    let idl_source_path = current_dir.join("res/drift.json");
+    let idl_mod_path = current_dir.join("crates/src/tangent_idl.rs");
+    generate_idl_types(&idl_source_path, idl_mod_path.as_path())?;
